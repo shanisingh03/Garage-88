be0f72ca15c9f7c125b830236c0dc9344aefc002
Sneha singh <mesnehasingh26@gmail.com>
2024-05-09 23:05:57 +0530

    Export Options Added to car services

--- app/Filament/Resources/CarServiceResource/Pages/ListCarServices.php
+++ app/Filament/Resources/CarServiceResource/Pages/ListCarServices.php
@@ -5,6 +5,9 @@ namespace App\Filament\Resources\CarServiceResource\Pages;
 use App\Filament\Resources\CarServiceResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use pxlrbt\FilamentExcel\Columns\Column;
+use pxlrbt\FilamentExcel\Exports\ExcelExport;
+use pxlrbt\FilamentExcel\Actions\Pages\ExportAction;
 
 class ListCarServices extends ListRecords
 {
@@ -14,6 +17,13 @@ class ListCarServices extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+            ExportAction::make() 
+                ->exports([
+                    ExcelExport::make()
+                        ->fromTable()
+                        ->withFilename(fn ($resource) => $resource::getModelLabel() . '-' . date('Y-m-d'))
+                        ->withWriterType(\Maatwebsite\Excel\Excel::CSV)                    
+                ]),
         ];
     }
 }
